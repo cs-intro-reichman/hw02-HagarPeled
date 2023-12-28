89b246e97ed1c87475329f1cd26985600257f866
HagarPeled <hagar.peled@post.runi.ac.il>
2023-12-28 15:36:36 +0200

    homework 2

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,28 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int N= Integer.parseInt(args[0]);
+		int SumDiv=0;
+		StringBuilder divisorString = new StringBuilder();
+		
+		for (int divisor= 1; divisor <=N / 2; divisor++)
+	      if (N%divisor==0){
+	         if (divisor != N) {
+	 	        if (divisorString.length() > 0) {
+                    divisorString.append(" + ");
+                    
+	 	    }
+	  }
+	  SumDiv += divisor;
+	  divisorString.append(divisor);
+	  }
+	  
+	  
+	  if (SumDiv==N) {
+	      System.out.println(N +" is a perfect number since " + N +" = " + divisorString);
+	  }
+	   else {
+	      System.out.println(N + " is not a perfect number");
 	}
+}
 }
