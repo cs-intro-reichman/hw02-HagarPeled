2a2372d42af4ad2b05469a4fdee607fb43b23370
HagarPeled <hagar.peled@post.runi.ac.il>
2023-12-28 16:48:37 +0200

    homework 2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,23 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
-	}
+
+        int n = Integer.parseInt(args[0]);
+        int i = 0;
+        int j;
+
+        while (i < n) {
+            j = 0;
+            if (i % 2 != 0) {
+                    System.out.print(" ");
+            }
+            while (j < n) {
+            
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println(); 
+            i++;
+        }
+    }
 }
