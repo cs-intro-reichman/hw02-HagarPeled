d113ece4d2264aea0bdb6cf054e6f55f884d5e1d
HagarPeled <hagar.peled@post.runi.ac.il>
2023-12-28 21:35:17 +0200

    homework 2

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,25 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+	    int boycount= 0;
+	    int girlcount=0;
+	    int totalchildren=0;
+		
+        do {
+            if (Math.random() < 0.5) {
+                System.out.print("b ");
+                boycount++;
+                totalchildren++;
+            }
+
+             else if (Math.random() > 0.5) {
+                System.out.print("g ");
+                girlcount++;
+                totalchildren++;
+            }
+            
+		} while (boycount < 1 || girlcount < 1);
+		
+	System.out.println("\nYou made it... and you now have  "+ totalchildren + " children.");	
 	}
 }
