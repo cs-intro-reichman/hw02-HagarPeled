408dcec681a39631b9647dc8fc091b5ef336c82b
HagarPeled <hagar.peled@post.runi.ac.il>
2023-12-29 00:03:01 +0200

    homework 2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -2,27 +2,25 @@
  *  Gets a command-line argument n (int), and prints an n-by-n damka board.
  */
 public class DamkaBoard {
-	public static void main(String[] args) {
-
+    public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int i = 0;
         int j;
 
         while (i < n) {
             j = 0;
-            if (i % 2 == 0) {
-                    System.out.print(" ");
-            }
             while (j < n) {
-            
-                System.out.print("*");
+                if (i % 2 == 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" *");
+                }
                 j++;
-               if (j < n ) {
-                    System.out.print(" ");
-                } 
             }
-            System.out.println(); 
+            System.out.println();
             i++;
         }
     }
 }
+
+    
